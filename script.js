097044273f6440437a471ee08029053eff61196c
deykpe <dpang2@uci.edu>
2020-02-21 16:50:28 -0800

    checking username with if / else statement

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+});
